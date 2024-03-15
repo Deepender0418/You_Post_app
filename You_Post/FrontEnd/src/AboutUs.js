828e21b1495afd514d35de30527040aec41cebb7
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2024-03-15 16:55:53 +0530

    Update AboutUs.js
    reformat_code

--- You_Post/FrontEnd/src/AboutUs.js
+++ You_Post/FrontEnd/src/AboutUs.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./css/about_us.css";
 
-export default function AboutUs() {
-  return (
-    <div className="about_div">
-      <h1>About us</h1>
-      <iframe
-        title="video"
-        src="https://drive.google.com/file/d/1DTH8D2rKBUSMQ75A_x7rsNQPVm4FzZgF/preview"
-        width="900"
-        height="500"
-        allow="autoplay"
-      />
-    </div>
-  );
+export default function AboutUs() 
+{
+    return (
+        <div className="about_div">
+            <h1>About us</h1>
+            <iframe
+                title="video"
+                src="https://drive.google.com/file/d/1DTH8D2rKBUSMQ75A_x7rsNQPVm4FzZgF/preview"
+                width="900"
+                height="500"
+                allow="autoplay"
+            />
+        </div>
+    );
 }
